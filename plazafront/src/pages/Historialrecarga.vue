8d6739fbcf2dd4b2db8e930d6843e93b41ae508a
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2023-02-12 19:46:38 -0400

    mod

--- plazafront/src/pages/Historialrecarga.vue
+++ plazafront/src/pages/Historialrecarga.vue
@@ -1,7 +1,7 @@
 <template>
 <q-page class="q-pa-xs">
   <div class=" responsive">
-    <table id="example" style="width:100%">
+    <table id="example" class="display" style="width:100%">
       <thead>
       <tr>
         <th>NRO</th>
@@ -14,12 +14,12 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(fila) in filas" :key="fila">
+      <tr v-for="fila in filas" :key="fila">
         <td>{{fila.num}}</td>
         <td>{{fila.fecha}}</td>
         <td>{{fila.hora}}</td>
         <td>{{fila.monto}}</td>
-        <td>{{fila.cliente.nombre}} {{fila.cliente.apellido}}</td>
+        <td>{{fila.nombre}} </td>
         <td>{{fila.usuario}}</td>
       </tr>
       </tbody>
@@ -47,285 +47,42 @@ export default {
     }
   },
   mounted() {
-      //  $('#example').DataTable().destroy()
-          $('#example').DataTable( {                      dom: 'Blfrtip',
-            // pageLength: 5,
-             lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],})
     this.misdatos()
-         // $('#example').DataTable( )
+    $('#example').DataTable( {
+      dom: 'Bfrtip',
+      buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'
+      ]
+    } );
   },
   methods:{
     misdatos(){
-      this.filas=[]
       this.$q.loading.show()
-        $('#example').DataTable().destroy()
+      this.filas=[]
       this.$axios.get(process.env.API+'/recarga').then( res=>{
-        //console.log(res.data);
+        // console.log(res.data);
         let num=0
         res.data.forEach(r => {
-        //console.log(r.user.name)
-         num++
-          r.num=num
-          r.usuario=r.user.name
-          this.filas.push(r)
-        })
-        //this.filas = res.data
-        $('#example').DataTable().destroy()
+          num++
+            r.num=num
+            r.usuario=r.user.name
+            r.nombre=r.cliente.nombre +' ' + r.cliente.apellido
+            this.filas.push(r)
+        });
+        
+        $('#example').DataTable().destroy();
         this.$nextTick(()=>{
           $('#example').DataTable( {
-                       "language":{
-              "processing": "Procesando...",
-              "lengthMenu": "Mostrar _MENU_ registros",
-              "zeroRecords": "No se encontraron resultados",
-              "emptyTable": "Ningún dato disponible en esta tabla",
-              "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-              "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-              "search": "Buscar:",
-              "infoThousands": ",",
-              "loadingRecords": "Cargando...",
-              "paginate": {
-                "first": "Primero",
-                "last": "Último",
-                "next": "Siguiente",
-                "previous": "Anterior"
-              },
-              "aria": {
-                "sortAscending": ": Activar para ordenar la columna de manera ascendente",
-                "sortDescending": ": Activar para ordenar la columna de manera descendente"
-              },
-              "buttons": {
-                "copy": "Copiar",
-                "colvis": "Visibilidad",
-                "collection": "Colección",
-                "colvisRestore": "Restaurar visibilidad",
-                "copyKeys": "Presione ctrl o u2318 + C para copiar los datos de la tabla al portapapeles del sistema. <br \/> <br \/> Para cancelar, haga clic en este mensaje o presione escape.",
-                "copySuccess": {
-                  "1": "Copiada 1 fila al portapapeles",
-                  "_": "Copiadas %ds fila al portapapeles"
-                },
-                "copyTitle": "Copiar al portapapeles",
-                "csv": "CSV",
-                "excel": "Excel",
-                "pageLength": {
-                  "-1": "Mostrar todas las filas",
-                  "_": "Mostrar %d filas"
-                },
-                "pdf": "PDF",
-                "print": "Imprimir",
-                "renameState": "Cambiar nombre",
-                "updateState": "Actualizar",
-                "createState": "Crear Estado",
-                "removeAllStates": "Remover Estados",
-                "removeState": "Remover",
-                "savedStates": "Estados Guardados",
-                "stateRestore": "Estado %d"
-              },
-              "autoFill": {
-                "cancel": "Cancelar",
-                "fill": "Rellene todas las celdas con <i>%d<\/i>",
-                "fillHorizontal": "Rellenar celdas horizontalmente",
-                "fillVertical": "Rellenar celdas verticalmentemente"
-              },
-              "decimal": ",",
-              "searchBuilder": {
-                "add": "Añadir condición",
-                "button": {
-                  "0": "Constructor de búsqueda",
-                  "_": "Constructor de búsqueda (%d)"
-                },
-                "clearAll": "Borrar todo",
-                "condition": "Condición",
-                "conditions": {
-                  "date": {
-                    "after": "Despues",
-                    "before": "Antes",
-                    "between": "Entre",
-                    "empty": "Vacío",
-                    "equals": "Igual a",
-                    "notBetween": "No entre",
-                    "notEmpty": "No Vacio",
-                    "not": "Diferente de"
-                  },
-                  "number": {
-                    "between": "Entre",
-                    "empty": "Vacio",
-                    "equals": "Igual a",
-                    "gt": "Mayor a",
-                    "gte": "Mayor o igual a",
-                    "lt": "Menor que",
-                    "lte": "Menor o igual que",
-                    "notBetween": "No entre",
-                    "notEmpty": "No vacío",
-                    "not": "Diferente de"
-                  },
-                  "string": {
-                    "contains": "Contiene",
-                    "empty": "Vacío",
-                    "endsWith": "Termina en",
-                    "equals": "Igual a",
-                    "notEmpty": "No Vacio",
-                    "startsWith": "Empieza con",
-                    "not": "Diferente de",
-                    "notContains": "No Contiene",
-                    "notStarts": "No empieza con",
-                    "notEnds": "No termina con"
-                  },
-                  "array": {
-                    "not": "Diferente de",
-                    "equals": "Igual",
-                    "empty": "Vacío",
-                    "contains": "Contiene",
-                    "notEmpty": "No Vacío",
-                    "without": "Sin"
-                  }
-                },
-                "data": "Data",
-                "deleteTitle": "Eliminar regla de filtrado",
-                "leftTitle": "Criterios anulados",
-                "logicAnd": "Y",
-                "logicOr": "O",
-                "rightTitle": "Criterios de sangría",
-                "title": {
-                  "0": "Constructor de búsqueda",
-                  "_": "Constructor de búsqueda (%d)"
-                },
-                "value": "Valor"
-              },
-              "searchPanes": {
-                "clearMessage": "Borrar todo",
-                "collapse": {
-                  "0": "Paneles de búsqueda",
-                  "_": "Paneles de búsqueda (%d)"
-                },
-                "count": "{total}",
-                "countFiltered": "{shown} ({total})",
-                "emptyPanes": "Sin paneles de búsqueda",
-                "loadMessage": "Cargando paneles de búsqueda",
-                "title": "Filtros Activos - %d",
-                "showMessage": "Mostrar Todo",
-                "collapseMessage": "Colapsar Todo"
-              },
-              "select": {
-                "cells": {
-                  "1": "1 celda seleccionada",
-                  "_": "%d celdas seleccionadas"
-                },
-                "columns": {
-                  "1": "1 columna seleccionada",
-                  "_": "%d columnas seleccionadas"
-                },
-                "rows": {
-                  "1": "1 fila seleccionada",
-                  "_": "%d filas seleccionadas"
-                }
-              },
-              "thousands": ".",
-              "datetime": {
-                "previous": "Anterior",
-                "next": "Proximo",
-                "hours": "Horas",
-                "minutes": "Minutos",
-                "seconds": "Segundos",
-                "unknown": "-",
-                "amPm": [
-                  "AM",
-                  "PM"
-                ],
-                "months": {
-                  "0": "Enero",
-                  "1": "Febrero",
-                  "10": "Noviembre",
-                  "11": "Diciembre",
-                  "2": "Marzo",
-                  "3": "Abril",
-                  "4": "Mayo",
-                  "5": "Junio",
-                  "6": "Julio",
-                  "7": "Agosto",
-                  "8": "Septiembre",
-                  "9": "Octubre"
-                },
-                "weekdays": [
-                  "Dom",
-                  "Lun",
-                  "Mar",
-                  "Mie",
-                  "Jue",
-                  "Vie",
-                  "Sab"
-                ]
-              },
-              "editor": {
-                "close": "Cerrar",
-                "create": {
-                  "button": "Nuevo",
-                  "title": "Crear Nuevo Registro",
-                  "submit": "Crear"
-                },
-                "edit": {
-                  "button": "Editar",
-                  "title": "Editar Registro",
-                  "submit": "Actualizar"
-                },
-                "remove": {
-                  "button": "Eliminar",
-                  "title": "Eliminar Registro",
-                  "submit": "Eliminar",
-                  "confirm": {
-                    "_": "¿Está seguro que desea eliminar %d filas?",
-                    "1": "¿Está seguro que desea eliminar 1 fila?"
-                  }
-                },
-                "error": {
-                  "system": "Ha ocurrido un error en el sistema (<a target=\"\\\" rel=\"\\ nofollow\" href=\"\\\">Más información&lt;\\\/a&gt;).<\/a>"
-                },
-                "multi": {
-                  "title": "Múltiples Valores",
-                  "info": "Los elementos seleccionados contienen diferentes valores para este registro. Para editar y establecer todos los elementos de este registro con el mismo valor, hacer click o tap aquí, de lo contrario conservarán sus valores individuales.",
-                  "restore": "Deshacer Cambios",
-                  "noMulti": "Este registro puede ser editado individualmente, pero no como parte de un grupo."
-                }
-              },
-              "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
-              "stateRestore": {
-                "creationModal": {
-                  "button": "Crear",
-                  "name": "Nombre:",
-                  "order": "Clasificación",
-                  "paging": "Paginación",
-                  "search": "Busqueda",
-                  "select": "Seleccionar",
-                  "columns": {
-                    "search": "Búsqueda de Columna",
-                    "visible": "Visibilidad de Columna"
-                  },
-                  "title": "Crear Nuevo Estado",
-                  "toggleLabel": "Incluir:"
-                },
-                "emptyError": "El nombre no puede estar vacio",
-                "removeConfirm": "¿Seguro que quiere eliminar este %s?",
-                "removeError": "Error al eliminar el registro",
-                "removeJoiner": "y",
-                "removeSubmit": "Eliminar",
-                "renameButton": "Cambiar Nombre",
-                "renameLabel": "Nuevo nombre para %s",
-                "duplicateError": "Ya existe un Estado con este nombre.",
-                "emptyStates": "No hay Estados guardados",
-                "removeTitle": "Remover Estado",
-                "renameTitle": "Cambiar Nombre Estado"
-              }
-            },
-                      dom: 'Blfrtip',
-            // pageLength: 5,
-             lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],
-          } )
-          this.$q.loading.hide()
+            dom: 'Bfrtip',
+            buttons: [
+              'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+          } );
+          this.$q.loading.hide();
         })
       }).catch(err=>{
-        this.$q.loading.hide()
-
+          this.$q.loading.hide();
         this.$q.notify({
-          
           message:err.message,
           color:'red',
           icon:'error'
